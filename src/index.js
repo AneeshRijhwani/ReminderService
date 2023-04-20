f48e8e85c7acfda1a100c6911170b2e09f52df73
AneeshRijhwani25 <7851950798aneesh@gmail.com>
2023-04-20 23:53:33 +0530

    Setup RabbitMq

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT} = require('./config/serverConfig');
 // const {sendBasicEmail} = require('./services/email-service');
-
+const { createChannel } = require('./utils/messageQueue');
 const jobs = require('./utils/job');
-const setupAndStartServe = () => {
+const setupAndStartServe = async() => {
     const app = express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
-
+    const channel = await createChannel();
     app.listen(PORT, () => {
         console.log(`Server Started at ${PORT}`);
         jobs();
