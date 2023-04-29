b80f64cdabfe44698b3d5d0f104cb30880025ecb
AneeshRijhwani25 <7851950798aneesh@gmail.com>
2023-04-29 23:51:17 +0530

    Completed message Queue and reminder service

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT} = require('./config/serverConfig');
-// const {sendBasicEmail} = require('./services/email-service');
-const { createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY} = require('./config/serverConfig');
+const TicketController = require('./controllers/ticket-controller');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
 const jobs = require('./utils/job');
-const setupAndStartServe = async() => {
+const EmailService = require('./services/email-service');
+const setupAndStartServer = async() => {
     const app = express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
+
+    app.post('api/v1/tickets', TicketController.create);
     const channel = await createChannel();
+    subscribeMessage(channel,EmailService.subscribeEvents,REMINDER_BINDING_KEY);
     app.listen(PORT, () => {
         console.log(`Server Started at ${PORT}`);
         jobs();
-        // sendBasicEmail(
-        //     'support@booking.com',
-        //     '7851950798aneesh@gmail.com',
-        //     'This is a test mail',
-        //     'Hey, how are you'
-        // )
+        
     })
 }
 
-setupAndStartServe();
+setupAndStartServer();
